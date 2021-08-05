47540124b0f4003a3c8b645cc4403d10de9be877
ohyr <ohyr96@ajou.ac.kr>
2021-08-05 13:57:12 +0900

    BOJ 2292번 벌집

---
+++ src/com/ohyr/boj/step8/problem02/Main.java
@@ -0,0 +1,23 @@
+package com.ohyr.boj.step8.problem02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		int sum = 1;
+		for(int i=0;i<=1000000000;i++) {
+			sum += 6*i;
+			if(sum >= n) {
+				System.out.println(i+1);
+				break;
+			}
+		}
+	}
+
+}
