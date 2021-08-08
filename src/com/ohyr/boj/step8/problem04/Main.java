0206bb92e4801ba296593d487ffb1cc5ef4968ec
ohyr <ohyr96@ajou.ac.kr>
2021-08-09 00:20:14 +0900

    BOJ 2869번 달팽이는 올라가고 싶다

---
+++ src/com/ohyr/boj/step8/problem04/Main.java
@@ -0,0 +1,22 @@
+package com.ohyr.boj.step8.problem04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+		
+		double answer = Math.ceil((double)(v - a) / (a - b)) + 1;
+		
+		System.out.println((int)answer);
+	}
+
+}
