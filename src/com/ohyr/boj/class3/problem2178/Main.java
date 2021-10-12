ee52753e5f796c7c63e1eafcbbd68014a7a16726
ohyr <ohyr96@ajou.ac.kr>
2021-10-12 22:25:49 +0900

    Class3 BOJ 2178

---
+++ src/com/ohyr/boj/class3/problem2178/Main.java
@@ -0,0 +1,68 @@
+package com.ohyr.boj.class3.problem2178;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] maze = new int[n][m];
+		
+		for(int i=0;i<n;i++) {
+			char[] rL = in.readLine().toCharArray();
+			for(int j=0;j<m;j++) {
+				maze[i][j] = rL[j]-'0';
+			}
+		}
+		
+		int[][] deltas = {{1,0},{0,1},{-1,0},{0,-1}};
+
+		int[][] visited = new int[n][m];
+		
+		Queue<Integer> qx = new LinkedList<>();
+		Queue<Integer> qy = new LinkedList<>();
+		Queue<Integer> qc = new LinkedList<>();
+		
+		qx.add(0);
+		qy.add(0);
+		qc.add(1);
+		int answer = 0;
+		while(!qx.isEmpty()) {
+			int x = qx.poll();
+			int y = qy.poll();
+			int cnt = qc.poll();
+
+			if(x < 0 || n <= x || y < 0 || m <= y) continue;
+			if(maze[x][y] == 0) continue;
+			if(visited[x][y] == 1) continue;
+			visited[x][y] = 1;
+			if(x == n-1 && y == m-1) {
+				answer = cnt;
+				break;
+			}
+			
+			for(int d=0;d<4;d++) {
+				int nx = x + deltas[d][0];
+				int ny = y + deltas[d][1];
+				
+				qx.add(nx);
+				qy.add(ny);
+				qc.add(cnt+1);
+			}
+		}
+		sb.append(answer);
+		System.out.println(sb.toString());
+	}
+
+}
