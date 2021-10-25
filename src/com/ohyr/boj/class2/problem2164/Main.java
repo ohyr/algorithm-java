8d54329cba128f9727a10e59abf3c42dc2f6a9dd
ohyr <ohyr96@ajou.ac.kr>
2021-10-25 21:56:10 +0900

    Class2 BOJ 2164

---
+++ src/com/ohyr/boj/class2/problem2164/Main.java
@@ -0,0 +1,35 @@
+package com.ohyr.boj.class2.problem2164;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i=1;i<=n;i++) {
+			q.offer(i);
+		}
+		
+		int r = 0;
+		while(q.size() > 1){
+			int tmp = q.poll();
+			
+			if(r%2 == 1) {
+				q.offer(tmp);
+			}
+
+			r++;
+		}
+		
+		System.out.println(q.poll());
+	}
+
+}
