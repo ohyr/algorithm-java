acdaa12df92ac09e02b834a78070bde19966faa8
ohyr <ohyr96@ajou.ac.kr>
2021-11-20 07:20:09 +0900

    Class3 BOJ 11399

---
+++ src/com/ohyr/boj/class3/problem11399/Main.java
@@ -0,0 +1,34 @@
+package com.ohyr.boj.class3.problem11399;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		int[] time = new int[n];
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<n;i++) {
+			time[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(time);
+		
+		int answer = 0;
+		
+		for(int i=0;i<n;i++) {
+			answer += time[i]*(n-i);
+		}
+		
+		System.out.println(answer);
+	}
+
+}
