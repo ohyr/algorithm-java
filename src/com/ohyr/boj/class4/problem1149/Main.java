8242a1d42ee1a3b6932c431e493d34bca6b196d5
ohyr <ohyr96@ajou.ac.kr>
2021-12-04 14:12:27 +0900

    Class4 BOJ 1149

---
+++ src/com/ohyr/boj/class4/problem1149/Main.java
@@ -0,0 +1,39 @@
+package com.ohyr.boj.class4.problem1149;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =null;
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		int[][] h = new int[n][3];
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(in.readLine());
+			h[i][0] = Integer.parseInt(st.nextToken());
+			h[i][1] = Integer.parseInt(st.nextToken());
+			h[i][2] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = Integer.MAX_VALUE;
+	
+		for(int i=1;i<n;i++) {
+			h[i][0] += h[i-1][1] < h[i-1][2] ? h[i-1][1] : h[i-1][2];
+			h[i][1] += h[i-1][0] < h[i-1][2] ? h[i-1][0] : h[i-1][2];
+			h[i][2] += h[i-1][0] < h[i-1][1] ? h[i-1][0] : h[i-1][1];
+		}
+		
+		for(int i=0;i<3;i++) {
+			answer = Math.min(answer, h[n-1][i]);
+		}
+	
+		System.out.println(answer);
+	}
+
+}
