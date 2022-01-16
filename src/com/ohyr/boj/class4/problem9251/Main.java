9e2dc60f855962128cc2291f5937378babeb8876
ohyr <ohyr96@ajou.ac.kr>
2022-01-16 22:34:45 +0900

    Class4 BOJ 9251

---
+++ src/com/ohyr/boj/class4/problem9251/Main.java
@@ -0,0 +1,29 @@
+package com.ohyr.boj.class4.problem9251;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String a = in.readLine();
+		String b = in.readLine();
+		
+		int[][] dp = new int[a.length()+1][b.length()+1];
+		
+		for(int i=1;i<=a.length();i++) {
+			for(int j=1;j<=b.length();j++) {
+				if(a.charAt(i-1) == b.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+		
+		System.out.println(dp[a.length()][b.length()]);
+	}
+
+}
