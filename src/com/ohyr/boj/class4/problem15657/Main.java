f5b8b09d82ce6f8aa926e47878e2a9a929815376
ohyr <ohyr96@ajou.ac.kr>
2022-02-03 22:36:26 +0900

    Class4 BOJ 15657

---
+++ src/com/ohyr/boj/class4/problem15657/Main.java
@@ -0,0 +1,50 @@
+package com.ohyr.boj.class4.problem15657;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static StringBuilder sb = new StringBuilder();
+	public static int[] numbers;
+	public static int[] idx;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[n];
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<n;i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numbers);
+		
+		idx = new int[m];
+		comb(0, 0, n, m);
+		
+		System.out.println(sb.toString());
+	}
+
+	private static void comb(int cnt, int start, int n, int r) {
+		if(cnt == r) {
+			for(int i=0;i<r;i++) {
+				sb.append(idx[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=start;i<n;i++) {
+			idx[cnt] = numbers[i];
+			comb(cnt+1, i, n, r);
+		}
+	}
+
+}
