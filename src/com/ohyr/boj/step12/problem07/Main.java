ecb9c3c52a1995bf9081456c094b69976b8d4614
ohyr <ohyr96@ajou.ac.kr>
2022-07-02 00:38:54 +0900

    단계별로 풀어보기 BOJ 11478 서로 다른 부분 문자열의 개수

---
+++ src/com/ohyr/boj/step12/problem07/Main.java
@@ -0,0 +1,23 @@
+package com.ohyr.boj.step12.problem07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String S = in.readLine();
+		
+		Set<String> set = new HashSet<>();
+		for(int i=1;i<=S.length();i++) {
+			for(int j=0;j<S.length() + 1 - i;j++) {
+				set.add(S.substring(j, j+i));
+			}
+		}
+		System.out.println(set.size());
+	}
+
+}
