a2cdefbbe84edccaf5f7682eb9285c3dd7401083
ohyr <ohyr96@ajou.ac.kr>
2022-07-09 22:36:05 +0900

    단계별로 풀어보기 BOJ 2981 검문

---
+++ src/com/ohyr/boj/step14/problem05/Main.java
@@ -0,0 +1,39 @@
+package com.ohyr.boj.step14.problem05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(in.readLine());
+		int[] num = new int[n];
+		for(int i=0;i<n;i++) {
+			num[i] = Integer.parseInt(in.readLine());
+		}
+		
+		int m = Math.abs(num[0] - num[1]);
+		for(int i=2;i<n;i++) {
+			m = gcd(m, Math.abs(num[i-1] - num[i]));
+		}
+		
+		for(int i=2;i<=m/2;i++) {
+			if(m % i == 0) {
+				sb.append(i).append(" ");
+			}
+		}
+		sb.append(m);
+		System.out.println(sb.toString());
+	}
+
+	public static int gcd(int a, int b) {
+		int r = a % b;
+		if(r == 0) {
+			return b;
+		}
+		return gcd(b, r);
+	}
+}
