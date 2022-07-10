5251cd0bf08997db221a4066cde4ae9e5e104f21
ohyr <ohyr96@ajou.ac.kr>
2022-07-10 21:41:46 +0900

    단계별로 풀어보기 BOJ 11050 이항 계수 1

---
+++ src/com/ohyr/boj/step14/problem07/Main.java
@@ -0,0 +1,30 @@
+package com.ohyr.boj.step14.problem07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int answer = 1;
+		
+		for(int i=0;i<k;i++) {
+			answer *= n-i;
+		}
+		
+		for(int i=k;i>0;--i) {
+			answer /= i;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
