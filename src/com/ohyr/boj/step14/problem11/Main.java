fa37c1f42be62d3947d0658067598f71c0202874
ohyr <ohyr96@ajou.ac.kr>
2022-07-13 22:14:51 +0900

    단계별로 풀어보기 BOJ 2004 조합 0의 개수

---
+++ src/com/ohyr/boj/step14/problem11/Main.java
@@ -0,0 +1,32 @@
+package com.ohyr.boj.step14.problem11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int a = cntFac(n, 5) - cntFac(n - m, 5) - cntFac(m, 5);
+		int b = cntFac(n, 2) - cntFac(n - m, 2) - cntFac(m, 2);
+		
+		System.out.println(Math.min(a, b));
+	}
+	
+	public static int cntFac(int n, int k) {
+		int cnt = 0;
+		while(n > 1) {
+			cnt += n / k;
+			n /= k;
+		}
+		return cnt;
+	}
+
+}
