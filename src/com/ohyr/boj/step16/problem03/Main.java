6454d099fa8e843b0f84f94e47f29f10fc6423e2
ohyr <ohyr96@ajou.ac.kr>
2022-07-16 21:15:34 +0900

    단계별로 풀어보기 BOJ 1904 01타일

---
+++ src/com/ohyr/boj/step16/problem03/Main.java
@@ -0,0 +1,20 @@
+package com.ohyr.boj.step16.problem03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		int[] dp = new int[n+2];
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i=3;i<=n;i++) {
+			dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+		}
+		System.out.println(dp[n]);
+	}
+
+}
