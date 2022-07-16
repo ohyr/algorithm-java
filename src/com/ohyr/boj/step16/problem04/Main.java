7ca95b018bf4f4d33936e865999bd907c3f7e2fc
ohyr <ohyr96@ajou.ac.kr>
2022-07-16 21:16:06 +0900

    단계별로 풀어보기 BOJ 9461 파도반 수열

---
+++ src/com/ohyr/boj/step16/problem04/Main.java
@@ -0,0 +1,29 @@
+package com.ohyr.boj.step16.problem04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		long[] P = new long[101];
+		P[1] = 1;
+		P[2] = 1;
+		P[3] = 1;
+		P[4] = 2;
+		for(int i=5;i<101;i++) {
+			P[i] = P[i-1] + P[i-5];
+		}
+		
+		int T = Integer.parseInt(in.readLine());
+		while(T-->0) {
+			int N = Integer.parseInt(in.readLine());
+			sb.append(P[N]).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
