35b8aef30a3fc1521f656151b4842910fb335225
ohyr <ohyr96@ajou.ac.kr>
2022-07-18 08:27:17 +0900

    단계별로 풀어보기 BOJ 2579 계단 오르기

---
+++ src/com/ohyr/boj/step16/problem08/Main.java
@@ -0,0 +1,27 @@
+package com.ohyr.boj.step16.problem08;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(in.readLine());
+		int[] steps = new int[n];
+		for(int i=0;i<n;i++) {
+			steps[i] = Integer.parseInt(in.readLine());
+		}
+		
+		int[][] dp = new int[n+2][2];
+
+		for(int i=2;i<n+2;i++) {
+			dp[i][0] = steps[i-2] + Math.max(dp[i-2][0], dp[i-2][1]);
+			dp[i][1] = steps[i-2] + dp[i-1][0];
+		}
+		
+		System.out.println(Math.max(dp[n+1][0], dp[n+1][1]));
+	}	
+
+}
