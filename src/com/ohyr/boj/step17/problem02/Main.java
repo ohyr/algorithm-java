0002d130400d7f2cc28cd1d12bc54db5a614c61d
ohyr <ohyr96@ajou.ac.kr>
2022-07-26 14:48:15 +0900

    단계별로 풀어보기 BOJ 2559 수열

---
+++ src/com/ohyr/boj/step17/problem02/Main.java
@@ -0,0 +1,39 @@
+package com.ohyr.boj.step17.problem02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] tempa = new int[n];
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<n;i++) {
+			tempa[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int sum = 0;
+		for(int i=0;i<k;i++) {
+			sum += tempa[i];
+		}
+		
+		int answer = sum;
+
+		for(int i=k;i<n;i++) {
+			sum += tempa[i];
+			sum -= tempa[i-k];
+			answer = Math.max(answer, sum);
+		}
+		
+		System.out.println(answer);
+	}
+
+}
