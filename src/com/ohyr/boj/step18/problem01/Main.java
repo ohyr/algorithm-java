66dad72c716687a60ff39a668da843ee36a7be6a
ohyr <ohyr96@ajou.ac.kr>
2022-07-29 17:35:12 +0900

    단계별로 풀어보기 BOJ 11047 동전 0

---
+++ src/com/ohyr/boj/step18/problem01/Main.java
@@ -0,0 +1,37 @@
+package com.ohyr.boj.step18.problem01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] a = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			a[i] = Integer.parseInt(in.readLine());
+		}
+		
+		int answer = 0;
+		
+		for(int i=n-1;i>=0;--i) {
+			int coinNum = k/a[i];
+			
+			if(coinNum == 0) continue;
+			
+			answer += coinNum;
+			k -= coinNum*a[i];
+		}
+		
+		System.out.println(answer);
+	}
+
+}
