60759c65e64da36095587394f59ee0e8d629308b
ohyr <ohyr96@ajou.ac.kr>
2022-07-30 10:58:10 +0900

    단계별로 풀어보기 BOJ 11399 ATM

---
+++ src/com/ohyr/boj/step18/problem03/Main.java
@@ -0,0 +1,34 @@
+package com.ohyr.boj.step18.problem03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		int[] time = new int[n];
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<n;i++) {
+			time[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(time);
+		
+		int answer = 0;
+		
+		for(int i=0;i<n;i++) {
+			answer += time[i]*(n-i);
+		}
+		
+		System.out.println(answer);
+	}
+
+}
