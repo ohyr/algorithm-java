e3af806ceb6e1a8b06b0e3ad3212167c663d414a
ohyr <ohyr96@ajou.ac.kr>
2022-07-31 20:19:10 +0900

    단계별로 풀어보기 BOJ 1541 잃어버린 괄호

---
+++ src/com/ohyr/boj/step18/problem04/Main.java
@@ -0,0 +1,33 @@
+package com.ohyr.boj.step18.problem04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int answer = 0;
+		
+		String s = in.readLine();
+		int pm = 1;
+		StringBuilder tmp = new StringBuilder();
+		for(int i=0;i<s.length();i++) {
+			char c = s.charAt(i);
+			if(c == '+' || c == '-') {
+				answer += pm * Integer.parseInt(tmp.toString());
+				tmp = new StringBuilder();
+				if(c == '-') {
+					pm = -1;
+				}
+			}else {
+				tmp.append(c);
+			}
+		}
+		answer += pm * Integer.parseInt(tmp.toString());
+		
+		System.out.println(answer);
+	}
+
+}
