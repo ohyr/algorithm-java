80718ba9f972eb3f4bfa2f60468ae95badbad778
ohyr <ohyr96@ajou.ac.kr>
2022-08-05 22:01:07 +0900

    단계별로 풀어보기 BOJ 11866 요세푸스 문제 0

---
+++ src/com/ohyr/boj/step20/problem03/Main.java
@@ -0,0 +1,42 @@
+package com.ohyr.boj.step20.problem03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1;i<=n;i++) {
+			q.offer(i);
+		}
+		
+		int cnt = 0;
+		while(!q.isEmpty()) {
+			cnt++;
+			if(cnt%k == 0) {
+				cnt = 0;
+				sb.append(q.poll()).append(", ");
+			}else {
+				q.offer(q.poll());
+			}
+		}
+		
+		sb.delete(sb.length()-2, sb.length());
+		sb.append(">");
+		System.out.println(sb.toString());
+	}
+
+}
