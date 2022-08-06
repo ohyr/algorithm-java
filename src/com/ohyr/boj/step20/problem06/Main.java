d58488a82aa31dba843dc92fb8da2c5d465e6a9e
ohyr <ohyr96@ajou.ac.kr>
2022-08-06 21:17:18 +0900

    단계별로 풀어보기 BOJ 1021 회전하는 큐

---
+++ src/com/ohyr/boj/step20/problem06/Main.java
@@ -0,0 +1,42 @@
+package com.ohyr.boj.step20.problem06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1;i<=n;i++) {
+			q.offer(i);
+		}
+		
+		int answer = 0;
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<m;i++) {
+			int cur = Integer.parseInt(st.nextToken());
+			int cnt = 0;
+			while(q.peek() != cur) {
+				q.offer(q.poll());
+				cnt++;
+			}
+			answer += Math.min(cnt, n - cnt);
+			q.poll();
+			n--;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
