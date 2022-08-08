c0733f2f1996eb909bd7314a7c423aa94e2fb2fa
ohyr <ohyr96@ajou.ac.kr>
2022-08-08 22:30:16 +0900

    단계별로 풀어보기 BOJ 2630 색종이 만들기

---
+++ src/com/ohyr/boj/step21/problem01/Main.java
@@ -0,0 +1,57 @@
+package com.ohyr.boj.step21.problem01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int[] WB = new int[2];
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		int[][] paper = new int[n][n];
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			for(int j=0;j<n;j++) {
+				paper[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		checkPaper(n,0,0,paper);
+		
+		sb.append(WB[0]).append("\n").append(WB[1]);
+		System.out.println(sb.toString());
+	}
+
+	private static void checkPaper(int n, int x, int y, int[][] paper) {
+		int init = paper[x][y];
+		
+		boolean perfect = true;
+		
+		out : for(int i=x;i<x+n;i++) {
+			for(int j=y;j<y+n;j++) {
+				if(init != paper[i][j]) {
+					perfect = false;
+					break out;
+				}
+			}
+		}
+		
+		if(perfect) {
+			WB[init]++;
+		}else {
+			for(int i=0;i<2;i++) {
+				for(int j=0;j<2;j++) {
+					checkPaper(n/2,x+n/2*i,y+n/2*j,paper);
+				}
+			}
+		}
+	}
+}
