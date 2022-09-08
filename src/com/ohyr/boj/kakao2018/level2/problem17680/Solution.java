520c9a4646fff22ee9c27cd1a7231b6c1b67d922
ohyr <ohyr96@ajou.ac.kr>
2022-09-08 18:47:30 +0900

    2018 KAKAO BLIND RECRUITMENT [1차] 캐시 Lv.2

---
+++ src/com/ohyr/boj/kakao2018/level2/problem17680/Solution.java
@@ -0,0 +1,34 @@
+package com.ohyr.boj.kakao2018.level2.problem17680;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        
+        List<String> cache = new ArrayList<>();
+        
+        for(String city: cities){
+            city = city.toLowerCase();
+            if(cache.contains(city)){
+                answer++;
+                for(int i=0;i<cache.size();i++){
+                    if(cache.get(i).equals(city)){
+                        cache.remove(i);
+                        break;
+                    }
+                }
+                cache.add(city);
+            }else{
+                answer += 5;
+                if(cacheSize == 0) continue;
+                if(cache.size() == cacheSize){
+                    cache.remove(0);
+                }
+                cache.add(city);
+            }
+        }
+        
+        return answer;
+    }
+}
