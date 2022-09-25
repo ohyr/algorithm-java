6148be30ca381175fc71730ebb28d57d6bd444b4
ohyr <ohyr96@ajou.ac.kr>
2022-09-25 21:56:45 +0900

    단계별로 풀어보기 BOJ 1676 팩토리얼 0의 개수

--- src/com/ohyr/boj/step14/problem11/Main.java
+++ src/com/ohyr/boj/step14/problem11/Main.java
@@ -2,31 +2,32 @@ package com.ohyr.boj.step14.problem11;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		
-		st = new StringTokenizer(in.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(in.readLine());
 		
-		int a = cntFac(n, 5) - cntFac(n - m, 5) - cntFac(m, 5);
-		int b = cntFac(n, 2) - cntFac(n - m, 2) - cntFac(m, 2);
+		int answer = 0;
 		
-		System.out.println(Math.min(a, b));
-	}
-	
-	public static int cntFac(int n, int k) {
-		int cnt = 0;
-		while(n > 1) {
-			cnt += n / k;
-			n /= k;
+		for(int i=5;i<=n;i+=5) {
+			int k = i/5;
+			answer++;
+			
+			while(k > 0) {
+				if(k % 5 == 0) {
+					answer++;
+				}else {
+					break;
+				}
+				
+				k /= 5;
+			}
 		}
-		return cnt;
+		
+		System.out.println(answer);
 	}
 
 }
