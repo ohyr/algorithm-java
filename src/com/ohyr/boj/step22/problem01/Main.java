a35cb0b6795695c23886b5314d321c1741613006
ohyr <ohyr96@ajou.ac.kr>
2022-09-26 21:44:03 +0900

    단계별로 풀어보기 BOJ 1920 수 찾기

---
+++ src/com/ohyr/boj/step22/problem01/Main.java
@@ -0,0 +1,41 @@
+package com.ohyr.boj.step22.problem01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		Set<Integer> set = new HashSet<>();
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<N;i++) {
+			int num = Integer.parseInt(st.nextToken());
+			set.add(num);
+		}
+		
+		int M = Integer.parseInt(in.readLine());
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<M;i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(set.contains(num)) {
+				sb.append(1).append("\n");
+			}else {
+				sb.append(0).append("\n");
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
