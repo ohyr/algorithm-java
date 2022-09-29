abedb137e888660ee7165e95b29c3a327b97ed9d
ohyr <ohyr96@ajou.ac.kr>
2022-09-29 23:50:09 +0900

    단계별로 풀어보기 BOJ 2805 나무 자르기

---
+++ src/com/ohyr/boj/step22/problem04/Main.java
@@ -0,0 +1,53 @@
+package com.ohyr.boj.step22.problem04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int max = 0;
+		
+		int[] trees = new int[n];
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<n;i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, trees[i]);
+		}
+		
+		int l = 0;
+		int r = max;
+		
+		while(l < r) {
+			int mid = (l + r) / 2;
+			
+			long sum = cutAndGetSum(mid, trees);
+			
+			if(sum >= m) {
+				l = mid + 1;
+			}else {
+				r = mid;
+			}
+		}
+		
+		System.out.println(l - 1);
+	}
+	
+	private static long cutAndGetSum(int h, int[] trees) {
+		long sum = 0;
+		
+		for(int tree: trees) {
+			sum += tree > h ? tree - h : 0;
+		}
+		
+		return sum;
+	}
+}
