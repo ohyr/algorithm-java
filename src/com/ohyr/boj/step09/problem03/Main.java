d8aea6f7c5d4cec63b124371ca4cc997b64b293e
ohyr <ohyr96@ajou.ac.kr>
2022-10-04 22:51:59 +0900

    단계별로 풀어보기 BOJ 25501 재귀의 귀재

---
+++ src/com/ohyr/boj/step09/problem03/Main.java
@@ -0,0 +1,38 @@
+package com.ohyr.boj.step09.problem03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int callCnt;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(in.readLine());
+		for(int tc=1;tc<=T;tc++) {
+			String S = in.readLine();
+			
+			callCnt = 0;
+			
+			sb.append(isPalindrome(S)).append(" ").append(callCnt).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	public static int isPalindrome(String S) {
+		return recursion(S, 0, S.length()-1);
+	}
+	
+	public static int recursion(String S, int l, int r) {
+		callCnt++;
+		
+		if(l >= r) return 1;
+		else if(S.charAt(l) != S.charAt(r)) return 0;
+		else return recursion(S, l+1, r-1);
+	}
+
+}
