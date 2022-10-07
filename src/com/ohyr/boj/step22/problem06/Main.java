01ae3de9eee82f75871f78fbaa6c2f86ec02bd03
ohyr <ohyr96@ajou.ac.kr>
2022-10-07 22:29:57 +0900

    단계별로 풀어보기 BOJ 1300 K번째 수

---
+++ src/com/ohyr/boj/step22/problem06/Main.java
@@ -0,0 +1,35 @@
+package com.ohyr.boj.step22.problem06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(in.readLine());
+		int k = Integer.parseInt(in.readLine());
+		
+		int l = 1;
+		int r = k;
+		
+		while(l < r) {
+			int mid = (l + r) / 2;
+			int count = 0;
+			
+			for(int i=1;i<=n;i++) {
+				count += Math.min(mid / i, n);
+			}
+			
+			if(k <= count) {
+				r = mid;
+			}else {
+				l = mid + 1;
+			}
+		}
+		
+		System.out.println(r);
+	}
+
+}
