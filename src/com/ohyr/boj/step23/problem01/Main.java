e8971a84e23af8b4d91124e65f687e44e94ddd11
ohyr <ohyr96@ajou.ac.kr>
2022-10-08 23:11:04 +0900

    단계별로 풀어보기 BOJ 11279 최대 힙

---
+++ src/com/ohyr/boj/step23/problem01/Main.java
@@ -0,0 +1,37 @@
+package com.ohyr.boj.step23.problem01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> (b - a));
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		for(int i=0;i<n;i++) {
+			int x = Integer.parseInt(in.readLine());
+			
+			switch(x) {
+			case 0:
+				if(q.isEmpty()) {
+					sb.append("0\n");
+				}else {
+					sb.append(q.poll()).append("\n");
+				}
+				break;
+			default:
+				q.offer(x);
+				break;
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
