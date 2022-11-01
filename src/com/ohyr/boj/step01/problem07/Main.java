9c7829dae470c983ccdc5d082fd3f15cbf9d76bc
ohyr <ohyr96@ajou.ac.kr>
2022-11-02 00:12:33 +0900

    단계별로 풀어보기 BOJ 10926 ??!

---
+++ src/com/ohyr/boj/step01/problem07/Main.java
@@ -0,0 +1,15 @@
+package com.ohyr.boj.step01.problem07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String id = in.readLine();
+		System.out.println(sb.append(id).append("??!").toString());
+	}
+
+}
