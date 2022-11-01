0c9939a573b34d680316770884b495fb7c58fb00
ohyr <ohyr96@ajou.ac.kr>
2022-11-02 00:12:56 +0900

    단계별로 풀어보기 BOJ 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/com/ohyr/boj/step01/problem08/Main.java
@@ -0,0 +1,14 @@
+package com.ohyr.boj.step01.problem08;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int y = Integer.parseInt(in.readLine());
+		System.out.println(y - 543);
+	}
+
+}
