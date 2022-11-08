9aaf99b063c65f54da95ce6c28436539608c9c53
ohyr <ohyr96@ajou.ac.kr>
2022-11-08 23:40:52 +0900

    단계별로 풀어보기 BOJ 13549 숨바꼭질 3

---
+++ src/com/ohyr/boj/step28/problem13549/Main.java
@@ -0,0 +1,51 @@
+package com.ohyr.boj.step28.problem13549;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int nkLimit = 200001;
+		
+		int[] checked = new int[nkLimit];
+		Arrays.fill(checked, Integer.MAX_VALUE);
+		
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {0, n});
+		
+		int answer = Integer.MAX_VALUE;
+		
+		while(!q.isEmpty()) {
+			int cnt = q.peek()[0];
+			int cur = q.peek()[1];
+			q.poll();
+			
+			if(cur < 0 || nkLimit <= cur) continue;
+			if(cur == k) {
+				answer = Math.min(answer, cnt);
+				continue;
+			}
+			
+			if(checked[cur] <= cnt) continue;
+			checked[cur] = cnt;
+			
+			q.offer(new int[] {cnt + 1, cur + 1});
+			q.offer(new int[] {cnt + 1, cur - 1});
+			if(cur < k) {				
+				q.offer(new int[] {cnt, 2 * cur});
+			}
+		}
+		
+		System.out.println(answer);
+	}
+
+}
