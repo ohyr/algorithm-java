566aab5ea0f199c55e90d5a47cba41a99c09d355
ohyr <ohyr96@ajou.ac.kr>
2022-11-09 23:58:28 +0900

    단계별로 풀어보기 BOJ 11404 플로이드

---
+++ src/com/ohyr/boj/step28/problem06/Main.java
@@ -0,0 +1,53 @@
+package com.ohyr.boj.step28.problem06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(in.readLine());
+		int m = Integer.parseInt(in.readLine());
+		
+		int[][] floyd = new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i == j) continue;
+				floyd[i][j] = Integer.MAX_VALUE/2;
+			}
+		}
+		
+		for(int i=0;i<m;i++) {
+			st = new StringTokenizer(in.readLine());
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			
+			floyd[a][b] = Math.min(floyd[a][b], c);
+		}
+		
+		for(int k=0;k<n;k++) {
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					floyd[i][j] = Math.min(floyd[i][j], floyd[i][k] + floyd[k][j]);
+				}
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(floyd[i][j] == Integer.MAX_VALUE/2) sb.append(0).append(" ");
+				else sb.append(floyd[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
