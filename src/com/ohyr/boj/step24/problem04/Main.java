9fbf9c41f454b8b73695cf607ab06b74730d1aa1
ohyr <ohyr96@ajou.ac.kr>
2022-12-12 23:28:20 +0900

    단계별로 풀어보기 BOJ 23757 아이들과 선물 상자

--- src/com/ohyr/boj/step24/problem04/Main.java
+++ src/com/ohyr/boj/step24/problem04/Main.java
@@ -3,38 +3,41 @@ package com.ohyr.boj.step24.problem04;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
 		
-		int n = Integer.parseInt(in.readLine());
+		int answer = 1;
 		
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
-		PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+		st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+		
+		st = new StringTokenizer(in.readLine());
 		for(int i=0;i<n;i++) {
-			int x = Integer.parseInt(in.readLine());
-			if(minHeap.size() == maxHeap.size()) {
-				if(!minHeap.isEmpty() && x > minHeap.peek()) {
-					maxHeap.add(minHeap.poll());
-					minHeap.add(x);
-				}else {					
-					maxHeap.add(x);
-				}
-			}else {
-				if(!maxHeap.isEmpty() && x < maxHeap.peek()) {
-					minHeap.add(maxHeap.poll());
-					maxHeap.add(x);
-				}else {					
-					minHeap.add(x);
-				}
-			}
-			sb.append(maxHeap.peek()).append("\n");
+			pq.offer(Integer.parseInt(st.nextToken()));
 		}
-		System.out.println(sb.toString());
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0;i<m;i++) {
+			int w = Integer.parseInt(st.nextToken());
+			int c = pq.poll();
+			
+			if(w > c) {
+				answer = 0;
+				break;
+			}
+			
+			pq.offer(c - w);
+		}
+		
+		System.out.println(answer);
 	}
 
 }
